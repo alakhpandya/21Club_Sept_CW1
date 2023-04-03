b8e46a4137c4584593c3548a641a292586e6194f
alakhpandya <alakhpandya@gmail.com>
2023-04-03 14:25:37 +0530

    apr3 commit

--- javascript/project5_todoList/main.js
+++ javascript/project5_todoList/main.js
@@ -1,50 +1,40 @@
 const master_container = document.querySelector('.master-container')
 const add_task_btn = document.querySelector('.btn');
 const form_input = document.querySelector('input');
-const task_container = document.createElement('div');
-var task_name;
-var check_icon_array = [];
-var trash_icon_array = [];
-var tasks_array = [];
+const task_container = document.querySelector('.task-container');
 
-task_container.setAttribute('class', 'task-container');
+let new_task, check_btn_array, trash_btn_array, target_btn;
 
 add_task_btn.addEventListener('click', () => {
-    if (master_container.childElementCount == 1){
-        master_container.appendChild(task_container);
-    }
-    
-    if (task_container.childElementCount >= 1){
-        var seperator = document.createElement('hr');
-        seperator.setAttribute('class', 'seperator');
-        task_container.appendChild(seperator);
-    }
-    var new_task = document.createElement('div');
+    new_task = document.createElement('div');
     new_task.setAttribute('class', 'new-task');
-    task_name = form_input.value;
-
-    var p = document.createElement('p');
-    p.innerText = `${task_name}`
-    var div = document.createElement('div');
-    `
+    new_task.innerHTML = `<p>${form_input.value}</p>
     <div>
-    <span class="icons"><i class="fa-solid fa-check check-icon"></i></span>
-    <span class="icons"><i class="fa-solid fa-trash trash-icon"></i></span>
-    </div>`;
+        <span class="icons check-btn"><i class="fa-solid fa-check"></i></span>
+        <span class="icons trash-btn"><i class="fa-solid fa-trash"></i></span>
+    </div>`
     task_container.appendChild(new_task);
+    check_btn_array = document.querySelectorAll('.check-btn');
+    trash_btn_array = document.querySelectorAll('.trash-btn');
+    // console.log(check_btn_array);
 })
 
-
-
-for(let i = 0; i < check_icon_array.length; i++){
-    check_icon_array[i].addEventListener('click', () => {
-        check_icon_array = document.getElementsByClassName('check-icon');
-        trash_icon_array = document.getElementsByClassName('trash-icon');
-        tasks_array = document.getElementsByClassName('new-task');
-        tasks_array[i].style.textDecoration = 'line-through';
-        console.log('event listened');
-    })
-}
-// if (master_container.childElementCount > 1){
-//     console.log('entered in if');
-// }
+task_container.addEventListener('click', (e) => {
+    target_btn = e.target;
+    if (target_btn.classList.contains('check-btn') || target_btn.classList.contains('fa-check')){
+        if(target_btn.classList.contains('fa-check')){
+            target_btn.parentElement.parentElement.previousElementSibling.style.textDecoration = 'line-through';
+        }
+        else{
+            target_btn.parentElement.previousElementSibling.style.textDecoration = 'line-through';
+        }
+    }
+    if (target_btn.classList.contains('trash-btn') || target_btn.classList.contains('fa-trash')){
+        if(target_btn.classList.contains('fa-trash')){
+            target_btn.parentElement.parentElement.parentElement.remove();
+        }
+        else{
+            target_btn.parentElement.parentElement.remove();
+        }
+    }
+})
